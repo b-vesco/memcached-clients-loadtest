5feadbdeae233483ed139f84244f22ff27fe6f66
Phillip Singer <psinger@playdom.com>
2012-09-11 12:10:35 -0700

    Adding a test for couchbase as well.

--- src/main/java/bbyk/loadtests/ClientFactory.java
+++ src/main/java/bbyk/loadtests/ClientFactory.java
@@ -1,112 +1,167 @@
 package bbyk.loadtests;
 
-import com.google.code.yanf4j.config.Configuration;
-import com.google.common.base.Function;
-import com.google.common.base.Throwables;
-import com.google.common.collect.Iterables;
-import com.whalin.MemCached.MemCachedClient;
-import com.whalin.MemCached.SockIOPool;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+
 import net.rubyeye.xmemcached.MemcachedClientBuilder;
 import net.rubyeye.xmemcached.XMemcachedClientBuilder;
 import net.spy.memcached.ConnectionFactoryBuilder;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.net.InetSocketAddress;
-import java.util.Arrays;
+import com.couchbase.client.CouchbaseClient;
+import com.couchbase.client.CouchbaseConnectionFactory;
+import com.couchbase.client.CouchbaseConnectionFactoryBuilder;
+import com.google.common.base.Function;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.whalin.MemCached.MemCachedClient;
+import com.whalin.MemCached.SockIOPool;
 
 /**
  * @author bbyk
  */
-public class ClientFactory {
-    private ClientSetup setup;
-    private InetSocketAddress[] addresses;
+public class ClientFactory
+{
+    private ClientSetup          setup;
+    private InetSocketAddress[]  addresses;
     private BasicMemcachedClient sharedClient;
 
-    public ClientFactory(@NotNull ClientSetup setup, @NotNull InetSocketAddress[] addresses) {
+    public ClientFactory(@NotNull ClientSetup setup, @NotNull InetSocketAddress[] addresses)
+    {
         this.setup = setup;
         this.addresses = addresses;
 
         SockIOPool pool = SockIOPool.getInstance();
-        final String[] strAddresses = Iterables.toArray(Iterables.transform(Arrays.asList(addresses),
-                new Function<InetSocketAddress, String>() {
-                    public String apply(InetSocketAddress address) {
-                        return address.getHostName() + ':' + address.getPort();
-                    }
-                }), String.class);
+        final String[] strAddresses = Iterables.toArray(Iterables.transform(Arrays.asList(addresses), new Function<InetSocketAddress, String>()
+        {
+            public String apply(InetSocketAddress address)
+            {
+                return address.getHostName() + ':' + address.getPort();
+            }
+        }), String.class);
         pool.setServers(strAddresses);
         pool.setSocketTO(2000); // whalin socket timeout
         pool.initialize();
     }
 
-    public BasicMemcachedClient getOrCreate() throws Exception {
-        if (setup.isShared) {
+    public BasicMemcachedClient getOrCreate() throws Exception
+    {
+        if (setup.isShared)
+        {
             if (sharedClient == null)
                 sharedClient = create();
             return sharedClient;
-        } else {
+        }
+        else
+        {
             return create();
         }
     }
 
-    private BasicMemcachedClient create() throws Exception {
+    private BasicMemcachedClient create() throws Exception
+    {
 
-        switch (setup) {
-            default:
-            case SHARED_ONE_SPY_MEMCACHED:
-            case PER_THREAD_SPY_MEMCACHED:
-                final ConnectionFactoryBuilder spyBuilder = new ConnectionFactoryBuilder();
-                spyBuilder.setTimeoutExceptionThreshold(2000);
-                spyBuilder.setOpTimeout(2000); // 2 sec
+        switch (setup)
+        {
+        default:
+        case SHARED_ONE_SPY_MEMCACHED:
+        case PER_THREAD_SPY_MEMCACHED:
+            final ConnectionFactoryBuilder spyBuilder = new ConnectionFactoryBuilder();
+            spyBuilder.setTimeoutExceptionThreshold(2000);
+            spyBuilder.setOpTimeout(2000); // 2 sec
 
-                return new BasicMemcachedClient() {
-                    final net.spy.memcached.MemcachedClient c = new net.spy.memcached.MemcachedClient(spyBuilder.build(),
-                            Arrays.asList(addresses));
+            return new BasicMemcachedClient()
+            {
+                final net.spy.memcached.MemcachedClient c = new net.spy.memcached.MemcachedClient(spyBuilder.build(), Arrays.asList(addresses));
 
-                    public byte[] get(@NotNull String key) {
+                public byte[] get(@NotNull String key)
+                {
+                    return (byte[]) c.get(key);
+                }
+
+                public void set(@NotNull String key, byte[] buffer)
+                {
+                    c.set(key, 0, buffer);
+                }
+            };
+        case SHARED_ONE_WHALIN:
+            return new BasicMemcachedClient()
+            {
+                final MemCachedClient c = new MemCachedClient();
+
+                public byte[] get(@NotNull String key)
+                {
+                    return (byte[]) c.get(key);
+                }
+
+                public void set(@NotNull String key, @Nullable byte[] buffer)
+                {
+                    c.set(key, buffer);
+                }
+            };
+        case SHARED_ONE_XMEMCACHED:
+            final MemcachedClientBuilder xbuilder = new XMemcachedClientBuilder(Arrays.asList(addresses));
+            xbuilder.setConnectionPoolSize(2);
+            xbuilder.getConfiguration().setSoTimeout(2000); // 2 sec
+
+            return new BasicMemcachedClient()
+            {
+                final net.rubyeye.xmemcached.MemcachedClient c = xbuilder.build();
+
+                public byte[] get(@NotNull String key)
+                {
+                    try
+                    {
                         return (byte[]) c.get(key);
                     }
+                    catch (Exception e)
+                    {
+                        throw Throwables.propagate(e);
+                    }
+                }
 
-                    public void set(@NotNull String key, byte[] buffer) {
+                public void set(@NotNull String key, @Nullable byte[] buffer)
+                {
+                    try
+                    {
                         c.set(key, 0, buffer);
                     }
-                };
-            case SHARED_ONE_WHALIN:
-                return new BasicMemcachedClient() {
-                    final MemCachedClient c = new MemCachedClient();
-
-                    public byte[] get(@NotNull String key) {
-                        return (byte[]) c.get(key);
+                    catch (Exception e)
+                    {
+                        throw Throwables.propagate(e);
                     }
+                }
+            };
 
-                    public void set(@NotNull String key, @Nullable byte[] buffer) {
-                        c.set(key, buffer);
-                    }
-                };
-            case SHARED_ONE_XMEMCACHED:
-                final MemcachedClientBuilder xbuilder = new XMemcachedClientBuilder(Arrays.asList(addresses));
-                xbuilder.setConnectionPoolSize(2);
-                xbuilder.getConfiguration().setSoTimeout(2000); // 2 sec
+        case SHARED_ONE_COUCHBASE:
+            final CouchbaseConnectionFactoryBuilder couchbaseBuilder = new CouchbaseConnectionFactoryBuilder();
+            couchbaseBuilder.setTimeoutExceptionThreshold(2000);
+            couchbaseBuilder.setOpTimeout(2000); // 2 sec
 
-                return new BasicMemcachedClient() {
-                    final net.rubyeye.xmemcached.MemcachedClient c = xbuilder.build();
+            // TODO: This is a gimpy hard-coded hack!!!
+            List<URI> couchbaseUris = Lists.newArrayList(new URI("http://localhost:8091/pools"));
 
-                    public byte[] get(@NotNull String key) {
-                        try {
-                            return (byte[]) c.get(key);
-                        } catch (Exception e) {
-                            throw Throwables.propagate(e);
-                        }
-                    }
+            final CouchbaseConnectionFactory connectionFactory = couchbaseBuilder.buildCouchbaseConnection(couchbaseUris, "default", "", "");
 
-                    public void set(@NotNull String key, @Nullable byte[] buffer) {
-                        try {
-                            c.set(key, 0, buffer);
-                        } catch (Exception e) {
-                            throw Throwables.propagate(e);
-                        }
-                    }
-                };
+            return new BasicMemcachedClient()
+            {
+                final CouchbaseClient c = new CouchbaseClient(connectionFactory);
+
+                public byte[] get(@NotNull String key)
+                {
+                    return (byte[]) c.get(key);
+                }
+
+                public void set(@NotNull String key, byte[] buffer)
+                {
+                    c.set(key, 0, buffer);
+                }
+            };
         }
     }
 }
--- src/main/java/bbyk/loadtests/ClientSetup.java
+++ src/main/java/bbyk/loadtests/ClientSetup.java
@@ -3,15 +3,19 @@ package bbyk.loadtests;
 /**
  * @author bbyk
  */
-public enum ClientSetup {
-    SHARED_ONE_SPY_MEMCACHED(true),
-    SHARED_ONE_WHALIN(true),
-    SHARED_ONE_XMEMCACHED(true),
-    PER_THREAD_SPY_MEMCACHED(false);
+public enum ClientSetup
+{
+    SHARED_ONE_COUCHBASE(true), //
+    SHARED_ONE_SPY_MEMCACHED(true), //
+    SHARED_ONE_WHALIN(true), //
+    SHARED_ONE_XMEMCACHED(true), //
+    PER_THREAD_SPY_MEMCACHED(false), //
+    ;
 
     boolean isShared;
 
-    ClientSetup(boolean shared) {
+    ClientSetup(boolean shared)
+    {
         this.isShared = shared;
     }
 }
